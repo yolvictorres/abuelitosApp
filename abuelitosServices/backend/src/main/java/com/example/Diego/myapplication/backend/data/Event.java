683df4dcad1e163c368f1a8394ef5d1bc16f3daa
SALA-10401\Ingenierias <s.gilparga@gmail.com>
2017-05-03 07:26:39 -0500

    correct entitiesand add entitty map

--- abuelitosServices/backend/src/main/java/com/example/Diego/myapplication/backend/data/Event.java
+++ abuelitosServices/backend/src/main/java/com/example/Diego/myapplication/backend/data/Event.java
@@ -1,8 +1,39 @@
 package com.example.Diego.myapplication.backend.data;
 
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
 /**
  * Created by astro on 2/05/2017.
  */
-
+@Entity
 public class Event {
+    @Id
+    private String id;
+    private String foto;
+    private String detalle;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detale) {
+        this.detalle = detale;
+    }
 }
